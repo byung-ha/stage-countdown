d7cee32979159014ec1307d893ba064134f6b68e
Byung Ha Lee <byungha.lee@autoscout24.com>
2025-01-27 09:36:13 +0100

    Adjust input size

--- src/App.tsx
+++ src/App.tsx
@@ -28,13 +28,14 @@ function App() {
         </div>
         <hr style={{color:'grey'}}/>
         <div className="control">
-          <input type='number' style={{width:'3em'}} value={minutes} size={2} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setMinutes(parseInt(e.target.value)??0)}/>:
+          <input type='number' style={{width:'3em'}} value={minutes} size={2} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setMinutes(parseInt(e.target.value)??0)}/>
+          <span>&nbsp;:&nbsp;</span>
           <input type='number' style={{width:'3em'}} value={seconds} size={2} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setSeconds(parseInt(e.target.value)??0)}/>
-          <button onClick={resetCountdown}>Start</button>
-          <input type='checkbox' checked={overtime} onChange={e => setOvertime(e.target.checked)}/>overtime
-          <input type='text' value={title} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}/>
-          count size:<input type='number' value={countdownSize} min={5} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setcountdownSize(parseInt(e.target.value)??0)}/>
-          title size:<input type='number' value={titleSize} min={5} onInput={(e:React.ChangeEvent<HTMLInputElement>) => settitleSize(parseInt(e.target.value)??0)}/>
+          <button onClick={resetCountdown}>Start</button>&nbsp;
+          <input type='checkbox' checked={overtime} onChange={e => setOvertime(e.target.checked)}/>overtime&nbsp;
+          <input type='text' value={title} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}/>&nbsp;
+          count size:<input type='number' style={{width:'3em'}} value={countdownSize} min={5} onInput={(e:React.ChangeEvent<HTMLInputElement>) => setcountdownSize(parseInt(e.target.value)??0)}/>&nbsp;
+          title size:<input type='number' style={{width:'3em'}} value={titleSize} min={5} onInput={(e:React.ChangeEvent<HTMLInputElement>) => settitleSize(parseInt(e.target.value)??0)}/>&nbsp;
         </div>
       </>
   )
